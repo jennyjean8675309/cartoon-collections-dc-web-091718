61065f030e63d01b963b253ae651d57b22305201
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-31 15:20:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find do |food|
+    food == cheese_types
+  end
 end
